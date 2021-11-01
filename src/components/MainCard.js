7330fdac0735798a54bb13008a52c10b000af862
Ruan Miguel <ruanmiguelps@gmail.com>
2021-11-01 09:58:36 -0300

    main card style

---
+++ src/components/MainCard.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Button from '../components/Button'
+function MainCard( { Style } ) {
+  return (
+    <div className={Style}>
+        <Button />
+    </div>
+  );
+}
+
+export default MainCard;
+
+MainCard.propTypes = {
+    Style: PropTypes.string.isRequired,
+  };
