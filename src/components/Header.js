2c9da214e5fab19dd1ef1c92a3184bd2b1c5e606
Ruan Miguel <ruanmiguelps@gmail.com>
2021-11-01 09:32:00 -0300

    [First-Draft] - Add Footer and Header

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function Header( { Style } ) {
+  return (
+    <div className={ Style }>
+      
+    </div>
+  );
+}
+
+export default Header;
+
+Header.propTypes = {
+    Style: PropTypes.string.isRequired,
+  };
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import Footer from '../components/Footer';
+import Header from '../components/Header';
+import '../Styles/Main.css'
 
 function Main() {
   return (
     <div>
-      "Hello World!!!"
+      <Header Style='header' />
+      <Footer Style='footer'/>
     </div>
   );
 }
